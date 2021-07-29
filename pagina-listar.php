23afdde3879942085db49020403d8b8b040aafc0
asheupdatesarg <85084493+asheupdatesarg@users.noreply.github.com>
2021-07-29 18:56:41 -0300

    Cambio

---
+++ pagina-listar.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300&display=swap" rel="stylesheet">
+	<title>LISTADO</title>
+		<link rel="stylesheet" type="text/css" href="estilo-listar.css">
+</head>
+<body>
+
+	<div class="content">
+			<h1 style="color:white;"> Listado de canciones </h1>
+     <a class="item" href="banderas.php?idCancion=1"><p>Till Forever Falls Apart</p></a>;
+	 <a class="item" href="banderas.php?idCancion=2"><p>I'm Fine</p></a>;
+	 <a class="item" href="banderas.php?idCancion=3"><p>Love Is Not Enough</p></a>;
+	 <a class="item" href="banderas.php?idCancion=4"><p>When I'm Older</p></a>;
+	 <a class="item" href="banderas.php?idCancion=5"><p>Me Without You</p></a>;
+	 <a class="item" href="banderas.php?idCancion=6"><p>Save Myself</p></a>;
+	 <a class="item" href="banderas.php?idCancion=7"><p>Taylor</p></a>;
+	 <a class="item" href="banderas.php?idCancion=8"><p>Not Mad Anymore</p></a>;
+	 <a class="item" href="banderas.php?idCancion=9"><p>Moral of the Story</p></a>;
+	 <a class="item" href="banderas.php?idCancion=10"><p>Serial Monogamist</p></a>;
+	 <a class="item" href="banderas.php?idCancion=11"><p>Always</p></a>;
+	 <a class="item" href="banderas.php?idCancion=12"><p>Ryne's Song</p></a>;
+	 <a class="item" href="banderas.php?idCancion=13"><p>Kansas</p></a>;
+	 <a class="item" href="banderas.php?idCancion=14"><p>Moral of the Story ft Niall Horan</p></a>;
+	</div>
+	<script src="scripts/script-listar.js"></script> 
+</body>
+</html>
